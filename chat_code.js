36571a23f2a7aeb723e9e58cdd351dfc26849ed7
jua15377 <jua15377@uvg.edu.gt>
2018-10-09 12:33:24 -0600

    enter to send, keep scroll, send messages

--- chat_code.js
+++ chat_code.js
@@ -1,19 +1,96 @@
-var result
-fetch('http://34.210.35.174:7000/')
+var oldMsjArray = []
+var newMsjArray = []
+const ulMsj = document.querySelector(".messages")
+const impNick = document.querySelector(".nickImput")
+const impid = document.querySelector(".idImput")
+const imptext = document.querySelector(".msjImput")
+const btnsend = document.querySelector("#btnSend").addEventListener("click", function(){
+  sentMsj(impid.value,impNick.value,imptext.value)
+})
+imptext.addEventListener("keypress", function(e){
+  var keyCode = e.keyCode;
+  if(keyCode == 13){
+      console.log("enter");
+      sentMsj(impid.value,impNick.value,imptext.value)
+  }
+})
+
+const msjArea = document.querySelector(".showmessages")
+
+const url = 'http://34.210.35.174:7000/'
+
+function getMsj(){
+  fetch(url)
   .then(function(response) {
-    result = response.json()
-    console.log(typeof result)
+    var result = response.json()
     return result
   })
   .then(function(myJson) {
-      var msj = document.querySelector(".messages")
-    myJson.forEach(function(element) {
-            updateList(msj, element.text)
+      myJson.forEach(function(element) {
+      const div = formatDisplayName(element.nick, element.student_id)
+      addToMsjListDisplay(div, element.text)
       })
+      msjArea.scrollTop = msjArea.scrollHeight;
   })
+}
 
-  function updateList(list, textToAdd){
+
+  function addToMsjListDisplay(identificador, textToAdd){
     var li = document.createElement("li");
+    li.appendChild(identificador)
+    li.appendChild(document.createElement("br"))
     li.appendChild(document.createTextNode(textToAdd))
-    list.appendChild(li)
+    ulMsj.appendChild(li)
   }
+
+  function formatDisplayName(user, id){
+    const div = document.createElement("div")
+    div.setAttribute("id", "nameAndId") 
+    const text = user + " - " + id
+    div.appendChild(document.createTextNode(text))
+    return div
+  }
+
+  function sentMsj(st_id, nick, text){
+
+    console.log(st_id)
+    console.log(nick)
+    console.log(text)
+
+    const data = new FormData();
+    if (st_id === "" || nick==="") {
+			alert('Please enter the nickname and id');
+			return;
+		}
+
+		// No message to send
+		if (text === "") {
+			alert('You must write a message');
+			return;
+    }
+    // max 140 character
+    if (text.length>140) {
+			alert('The message have to be shorter than 140 characters');
+			return;
+    }
+    data.append('student_id', st_id)
+    data.append('text', text)
+    data.append('nick', nick)
+    const otherParam={
+      method:"POST",
+      body: data
+    }
+    fetch(url,otherParam).then(function(){
+      const div = formatDisplayName(nick, st_id)
+      addToMsjListDisplay(div, text)
+      imptext.value = ""
+      msjArea.scrollTop = msjArea.scrollHeight;
+    }).catch(function(){
+        aleer("Error Sending the message")
+    })
+  }
+
+  setInterval(function() {
+    // method to be executed;
+    getMsj()
+  }, 1000);
